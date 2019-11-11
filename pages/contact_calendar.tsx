b304db2c1273bccb8022c55d087964720bee59e8
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-11-11 18:30:09 -0300

    BugFix agenda

--- pages/contact_calendar.tsx
+++ pages/contact_calendar.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 // import { useSelector } from 'react-redux'
 import Link from 'next/link'
 import { StoreData } from '../redux/store'
-
+import businessService from '../services/business'
 
 export default () => {
   const router = useRouter()
   const { goBackTo } = router.query
+  const bs = businessService
+
   // const user = useSelector((state: StoreData) => state.loggedUser.user)
 
-  const goBack = () => router.push(goBackTo as string)
+  useEffect(() => {
+    const fetchContacts = async () => {
+
+    }
+  }, [])
 
   return (
     <div>
