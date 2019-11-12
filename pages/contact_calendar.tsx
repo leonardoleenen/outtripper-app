077d510980bd2af2ae61a1768f31c5195c6556e8
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-11-12 17:30:56 -0300

    adding contact list

--- pages/contact_calendar.tsx
+++ pages/contact_calendar.tsx
@@ -1,26 +1,31 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
-// import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+
 import Link from 'next/link'
+import { useSelector } from 'react-redux'
 import { StoreData } from '../redux/store'
 import businessService from '../services/business'
 
 export default () => {
-  const router = useRouter()
-  const { goBackTo } = router.query
   const bs = businessService
-
-  // const user = useSelector((state: StoreData) => state.loggedUser.user)
+  const { user } = useSelector((state : StoreData) => state.loggedUser)
+  const [contactList, setContactList] = useState([])
 
   useEffect(() => {
     const fetchContacts = async () => {
-
+      setContactList(await bs.getContacts(user.organization))
     }
+
+    fetchContacts()
   }, [])
 
   return (
     <div>
       <div>Lista </div>
+      {contactList.map((c:Contact) => (
+        <div key={c.id}>
+          {c.firstName}
+        </div>
+      ))}
       <Link href="/new_contact">
         <div> New</div>
       </Link>
